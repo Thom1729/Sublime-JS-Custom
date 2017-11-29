d0f89d508b7d0cd2fc870c32bde7316431d43f7c
Thomas Smith <thom@Thomass-iMac.lan>
2017-11-29 00:32:33 -0500

    More work. Moved compiled syntaxes to User directory.

--- build_custom_syntaxes.py
+++ build_custom_syntaxes.py
@@ -8,15 +8,33 @@ from YAMLMacros.src.build import build_yaml_macros
 
 ROOT = path.dirname(__file__)
 SOURCE_PATH = path.join(ROOT, 'JS Custom.sublime-syntax.yaml-macros')
-SYNTAXES_PATH = target = path.join(ROOT, 'syntaxes')
+# SYNTAXES_PATH = target = path.join(ROOT, 'syntaxes')
+SYNTAXES_PATH = target = path.join(sublime.packages_path(), 'User', 'JS Custom')
+
+if not os.path.exists(SYNTAXES_PATH):
+    os.makedirs(SYNTAXES_PATH)
+    sublime.run_command('build_js_custom_syntaxes')
 
 SETTINGS = sublime.load_settings('JS Custom.sublime-settings')
 
-old_configurations = SETTINGS.get('configurations')
+def merge(*dicts):
+    ret = {}
+    for d in dicts:
+        ret.update(d)
+    return ret
+
+def get_configurations():
+    defaults = SETTINGS.get('defaults')
+    return {
+        name: merge(defaults, config)
+        for name, config in SETTINGS.get('configurations').items()
+    }
+
+old_configurations = get_configurations()
 def rebuild_syntaxes():
     global old_configurations
 
-    new_configurations = SETTINGS.get('configurations')
+    new_configurations = get_configurations()
     
     changed = [
         name
@@ -36,7 +54,7 @@ class BuildJsCustomSyntaxesCommand(sublime_plugin.ApplicationCommand):
     def run(self, versions=None):
         os.chdir(ROOT)
 
-        configurations = SETTINGS.get('configurations')
+        configurations = get_configurations()
         if not versions:
             versions = [ name for name, _ in configurations.items() ]
 
