f69b34edc3d6e6e771e45ea8d81541d44fb340c7
Thomas Smith <thom@Thomass-iMac.lan>
2018-03-11 14:23:34 -0400

    Added setting to enable/disable overriding the built-in close_tag command.

--- jsx_close_tag_listener.py
+++ jsx_close_tag_listener.py
@@ -5,5 +5,6 @@ class JsxCloseTagListener(sublime_plugin.ViewEventListener):
     def on_text_command(self, command_name, args):
         if command_name != 'close_tag': return
         if not self.view.match_selector(0, 'source.js'): return
+        if not sublime.load_settings('JS Custom.sublime-settings').get('jsx_close_tag', False): return
 
         return ('jsx_close_tag', args)
