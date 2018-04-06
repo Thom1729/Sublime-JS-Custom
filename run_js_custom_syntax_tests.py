971ff3a5aec8f97345ee4370fb1d8ac1b3424c70
thom <thom@localhost.localdomain>
2018-04-06 14:48:18 -0400

    Refactored syntax test runner.

--- run_js_custom_syntax_tests.py
+++ run_js_custom_syntax_tests.py
@@ -3,7 +3,6 @@ import sublime_plugin
 
 import os
 from os import path
-from itertools import groupby
 
 from .src.output import OutputPanel
 from .src.build import build_configuration
@@ -33,6 +32,27 @@ def run_syntax_tests(tests, output):
     output.print(message.format(*params))
     output.print()
 
+def run_tests_for_configuration(name, configuration, output, tests):
+    p = system_path(TEST_PATH, name)
+
+    os.makedirs(p)
+
+    build_configuration(name, configuration, p, output)
+
+    syntax_path = resource_path(TEST_PATH, name, name+'.sublime-syntax')
+
+    for test in tests:
+        with open(system_path(TEST_PATH, name, test['filename']), 'w') as file:
+            file.write('// SYNTAX TEST "%s"\n' % syntax_path)
+            file.write(test['contents'])
+
+    test_paths = [
+        resource_path(TEST_PATH, name, test['filename'])
+        for test in tests
+    ]
+
+    run_syntax_tests(test_paths, output)
+
 class RunJsCustomSyntaxTestsCommand(sublime_plugin.WindowCommand):
     def run(self):
         clean_tests()
@@ -51,30 +71,11 @@ class RunJsCustomSyntaxTestsCommand(sublime_plugin.WindowCommand):
             if file_path.startswith('Packages/JSCustom/tests')
         ]
 
-        syntax_tests.sort(key=lambda test: ( test['suite'], test['filename'] ))
-
-        suites = {
-            k: list(v)
-            for k,v in groupby(syntax_tests, key=lambda test:test['suite'])
-        }
-
         for name, case in cases.items():
-            p = system_path(TEST_PATH, name)
-            
-            os.makedirs(p)
+            tests = [
+                test
+                for test in syntax_tests
+                if test['suite'] in case['tests']
+            ]
 
-            build_configuration(name, case['configuration'], p, output)
-
-            syntax_path = resource_path(TEST_PATH, name, name+'.sublime-syntax')
-
-            tests = []
-
-            for suite_name in case['tests']:
-                for test in suites[suite_name]:
-                    with open(system_path(TEST_PATH, name, test['filename']), 'w') as file:
-                        file.write('// SYNTAX TEST "%s"\n' % syntax_path)
-                        file.write(test['contents'])
-
-                    tests.append(resource_path(TEST_PATH, name, test['filename']))
-            
-            run_syntax_tests(tests, output)
+            run_tests_for_configuration(name, case['configuration'], output, tests)
