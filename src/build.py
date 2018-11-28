dc7a2801146a12680aa4d266857a0e21c1c67be1
Thomas Smith <thom@Thomass-iMac.lan>
2018-11-27 22:29:07 -0500

    Atomic replacement of syntaxes.

--- src/build.py
+++ src/build.py
@@ -3,6 +3,7 @@ import re
 from .paths import PACKAGE_PATH
 from .util import merge
 from .defer import defer_each
+from .atomic import atomic_replace
 
 
 __all__ = ['build_configurations']
@@ -19,16 +20,18 @@ def build_configurations(configurations, destination_path, output=None):
     def _build(value):
         name, configuration = value
 
-        d = destination_path / (name + '.sublime-syntax')
-        build(
-            source_text=source_text,
-            destination_path=str(d.file_path()),
-            arguments=merge({
-                'name': 'JS Custom - %s' % name,
-                'scope': 'source.js.%s' % re.sub(r'[^\w-]', '', name.lower()),
-                'file_path': str(SOURCE_PATH.file_path()),
-            }, configuration),
-            error_stream=output,
-        )
+        with atomic_replace(
+            (destination_path / (name + '.sublime-syntax')).file_path()
+        ) as temp:
+            build(
+                source_text=source_text,
+                destination_path=temp.name,
+                arguments=merge({
+                    'name': 'JS Custom - %s' % name,
+                    'scope': 'source.js.%s' % re.sub(r'[^\w-]', '', name.lower()),
+                    'file_path': str(SOURCE_PATH.file_path()),
+                }, configuration),
+                error_stream=output,
+            )
 
     defer_each(_build, configurations.items())
