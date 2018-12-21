00e9e45eac44e60fe713fda44fb273795912a18e
Thomas Smith <thom@Thomass-iMac.lan>
2018-12-20 22:15:03 -0500

    Compatibility with new macro engine.

--- src/macros.py
+++ src/macros.py
@@ -1,24 +1,31 @@
 from yamlmacros.lib.extend import apply
 from yamlmacros.lib.syntax import rule as _rule
+from yamlmacros.lib.include import include_resource
 
 from yamlmacros import process_macros
 
 import sublime
 from os import path
 
-def include_resource(resource):
-    file_contents = sublime.load_resource(resource)
-    return process_macros(
-        file_contents,
-        arguments={ "file_path": resource },
-    )
+
+__all__ = ['apply', 'include_resource', 'get_extensions']
+
+
+def merge(*args):
+    ret = {}
+    for arg in args:
+        ret.update(arg)
+    return ret
 
 def has_value(val):
     return val is not None and val is not False
 
 def get_extensions(node, eval, arguments):
     return [
-        include_resource(file_path)
+        process_macros(
+            sublime.load_resource(file_path),
+            arguments=merge(arguments, { "file_path": file_path }),
+        )
         for file_path in sublime.find_resources('*.yaml')
         if path.dirname(file_path).endswith('Packages/JSCustom/extensions')
         and has_value(arguments.get(path.splitext(path.basename(file_path))[0], None))
