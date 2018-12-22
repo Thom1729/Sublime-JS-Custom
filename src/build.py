081d8feaf8d52bc5ba7d8883e537c718edd86589
Thomas Smith <thom@Thomass-iMac.lan>
2018-12-22 10:23:44 -0500

    Use explicit threading.

--- src/build.py
+++ src/build.py
@@ -1,8 +1,8 @@
 import re
+from threading import Thread
 
 from .paths import PACKAGE_PATH
 from .util import merge
-from .defer import defer_each
 from .atomic import atomic_replace
 
 
@@ -13,25 +13,23 @@ SOURCE_PATH = PACKAGE_PATH / 'src/syntax/JS Custom.sublime-syntax.yaml-macros'
 
 
 def build_configurations(configurations, destination_path, output=None):
-    from yamlmacros import build
+    def run():
+        from yamlmacros import build
 
-    source_text = SOURCE_PATH.read_text()
+        source_text = SOURCE_PATH.read_text()
 
-    def _build(value):
-        name, configuration = value
-
-        with atomic_replace(
-            (destination_path / (name + '.sublime-syntax')).file_path()
-        ) as temp:
-            build(
-                source_text=source_text,
-                destination_path=temp.name,
-                arguments=merge({
-                    'name': 'JS Custom - %s' % name,
-                    'scope': 'source.js.%s' % re.sub(r'[^\w-]', '', name.lower()),
-                    'file_path': str(SOURCE_PATH.file_path()),
-                }, configuration),
-                error_stream=output,
-            )
-
-    defer_each(_build, configurations.items())
+        for name, configuration in configurations.items():
+            with atomic_replace(
+                (destination_path / (name + '.sublime-syntax')).file_path()
+            ) as temp:
+                build(
+                    source_text=source_text,
+                    destination_path=temp.name,
+                    arguments=merge({
+                        'name': 'JS Custom - {}'.format(name),
+                        'scope': 'source.js.{}'.format(re.sub(r'[^\w-]', '', name.lower())),
+                        'file_path': str(SOURCE_PATH.file_path()),
+                    }, configuration),
+                    error_stream=output,
+                )
+    Thread(target=run).start()
