dbcfa7165e922621e1c63ad2200675dafea5a398
Thomas Smith <thom@Thomass-iMac.lan>
2018-12-22 10:39:36 -0500

    Simplified extensions.

--- src/syntax/macros.py
+++ src/syntax/macros.py
@@ -4,32 +4,19 @@ from yamlmacros import process_macros
 from yamlmacros.src.util import merge
 from yamlmacros.lib.include import include_resource  # noqa: F401
 
-import sublime
-from os import path
+from sublime_lib import ResourcePath
 
 
 __all__ = ['apply', 'include_resource', 'get_extensions']
 
 
-def _include_resource(file_path, loader):
-    file_contents = sublime.load_resource(file_path)
-    return process_macros(
-        file_contents,
-        arguments=merge(loader.context, {"file_path": file_path}),
-    )
-
-
-def has_value(val):
-    return val is not None and val is not False
-
-
-def get_extensions(node, loader):
+def get_extensions(path):
+    arguments = (yield).context
     return [
-        _include_resource(file_path, loader)
-        for file_path in sublime.find_resources('*.yaml')
-        if path.dirname(file_path).endswith('Packages/JSCustom/extensions') and
-        has_value(loader.context.get(path.splitext(path.basename(file_path))[0], None))
+        process_macros(
+            file_path.read_text(),
+            arguments=merge(arguments, {"file_path": str(file_path)}),
+        )
+        for file_path in ResourcePath.glob_resources(path)
+        if arguments.get(file_path.stem) not in (None, False)
     ]
-
-
-get_extensions.raw = True
