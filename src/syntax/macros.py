e852e81eb31f9ac9c4ed2bedceeccc0e550b7ba1
Thomas Smith <thom@Thomass-iMac.lan>
2018-12-22 23:58:51 -0500

    Simplified base macros.

--- src/syntax/macros.py
+++ src/syntax/macros.py
@@ -1,7 +1,4 @@
 from yamlmacros.lib.extend import apply
-
-from yamlmacros import process_macros
-from yamlmacros.src.util import merge
 from yamlmacros.lib.include import include_resource  # noqa: F401
 
 from sublime_lib import ResourcePath
@@ -12,11 +9,9 @@ __all__ = ['apply', 'include_resource', 'get_extensions']
 
 def get_extensions(path):
     arguments = (yield).context
-    return [
-        process_macros(
-            file_path.read_text(),
-            arguments=merge(arguments, {"file_path": str(file_path)}),
-        )
-        for file_path in ResourcePath.glob_resources(path)
-        if arguments.get(file_path.stem) not in (None, False)
-    ]
+    ret = []
+    for file_path in ResourcePath.glob_resources(path):
+        if arguments.get(file_path.stem) not in (None, False):
+            ret.append((yield from include_resource(str(file_path))))
+
+    return ret
