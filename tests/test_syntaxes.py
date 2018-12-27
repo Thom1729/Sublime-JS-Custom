ba655322e7883697f16408452b00dcfe27df35c1
Thomas Smith <thom@Thomass-iMac.lan>
2018-12-27 12:46:02 -0500

    Simplified syntax tests.

--- tests/test_syntaxes.py
+++ tests/test_syntaxes.py
@@ -8,6 +8,7 @@ from JSCustom.src.paths import PACKAGE_PATH, USER_DATA_PATH
 
 
 TESTS_PATH = USER_DATA_PATH / 'Tests'
+TEST_SUITES_PATH = PACKAGE_PATH / 'tests/syntax_test_suites'
 
 
 class TestSyntaxes(DeferrableTestCase):
@@ -18,37 +19,33 @@ class TestSyntaxes(DeferrableTestCase):
         shutil.rmtree(str(TESTS_PATH.file_path()), ignore_errors=True)
         TESTS_PATH.file_path().mkdir(parents=True)
 
-        cls.all_tests = PACKAGE_PATH.glob('tests/**/syntax_test*')
+        cls.all_tests = TEST_SUITES_PATH.rglob('syntax_test*')
 
     def _test_syntaxes(self, name, configuration, tests):
         test_working_path = TESTS_PATH / name
         test_working_path.file_path().mkdir(parents=True)
 
-        test_source_paths = [
-            (test_working_path / test_path.name, test_path.read_text())
-            for test_path in self.all_tests
-            if test_path.parent.name in tests
-        ]
-
         build_configurations({name: configuration}, test_working_path)
-
         syntax_path = test_working_path / (name + '.sublime-syntax')
-
-        for path, text in test_source_paths:
-            with path.file_path().open('w', encoding='utf-8') as file:
-                file.write('// SYNTAX TEST "{!s}"\n{}'.format(
-                    syntax_path,
-                    text
-                ))
 
         yield syntax_path.exists
         yield 200  # Hope this gives Sublime long enough to compile it.
 
+        syntax_test_header = '// SYNTAX TEST "{!s}"\n'.format(syntax_path)
         all_failures = []
-        for path, text in test_source_paths:
-            yield path.exists
-            assertion_count, failures = sublime_api.run_syntax_test(str(path))
-            all_failures.extend(failures)
+
+        for test_source in self.all_tests:
+            if test_source.parent.name in tests:
+                test_dest = test_working_path / test_source.name
+                text = test_source.read_text().split('\n', 1)[1]
+
+                with test_dest.file_path().open('w', encoding='utf-8') as file:
+                    file.write(syntax_test_header)
+                    file.write(text)
+
+                yield test_dest.exists
+                assertion_count, failures = sublime_api.run_syntax_test(str(test_dest))
+                all_failures.extend(failures)
 
         self.assertEqual(all_failures, [])
 
