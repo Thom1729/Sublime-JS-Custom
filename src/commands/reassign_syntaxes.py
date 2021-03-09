5d2d2e6c348e3d2565c6e754c0e4900e7448432c
Thom Smith <thom@thomthesmith.com>
2021-03-08 19:18:09 -0500

    Use assign_syntax in ReassignSyntaxesCommand.

--- src/commands/reassign_syntaxes.py
+++ src/commands/reassign_syntaxes.py
@@ -8,10 +8,6 @@ class ReassignSyntaxesCommand(sublime_plugin.ApplicationCommand):
     def run(self, syntaxes, replacement):
         for window in sublime.windows():
             for view in window.views():
-                settings = view.settings()
-                if settings.get('syntax') in syntaxes:
-                    print('replacing {} with {}'.format(
-                        settings.get('syntax'),
-                        replacement
-                    ))
-                    settings.set('syntax', replacement)
+                syntax = view.settings().get('syntax')
+                if syntax in syntaxes:
+                    view.assign_syntax(replacement)
