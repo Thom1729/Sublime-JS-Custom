ce20f0bfb6594fd44477e285ffdf256faec6f508
Thom Smith <thom@thomthesmith.com>
2021-05-06 22:48:08 -0400

    Fix paths issue when installed.

--- src/paths.py
+++ src/paths.py
@@ -4,4 +4,5 @@ from sublime_lib import ResourcePath
 __all__ = ['USER_DATA_PATH', 'PACKAGE_PATH']
 
 USER_DATA_PATH = ResourcePath('Packages/User/JS Custom')
-PACKAGE_PATH = ResourcePath.from_file_path(__file__).parent.parent
+PACKAGE_PATH = ResourcePath('Packages/JSCustom')
+# PACKAGE_PATH = ResourcePath.from_file_path(__file__).parent.parent
